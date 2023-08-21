34878cc9c95b0e0c6bacaa907cabfcf6e77cd80e
neruneruna7 <113534341+neruneruna7@users.noreply.github.com>
2023-08-21 22:49:54 +0900

    update: 使いやすく改修

--- data_processing/src/lib.rs
+++ data_processing/src/lib.rs
@@ -1,3 +1,4 @@
+
 // {
 //     "index": "0",
 //     "message_tree_id": "6ab24d72-0181-4594-a9cd-deaf170242fb",
@@ -10,76 +11,49 @@
 //     "ng_translation": "0.0",
 //     "use_deepl": 0
 // },
-// このjsonを格納する構造体を作成する
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Message {
-    index: String,
-    message_tree_id: String,
-    message_id: String,
-    parent_id: String,
-    text: String,
-    text_ja: String,
-    role: String,
-    lang: String,
-    ng_translation: String,
-    use_deepl: i64,
-}
 
 use std::fs::File;
 use std::io::{Read, Write};
 use serde::{Deserialize, Serialize};
 
+pub fn json_get_text(output_path: &str, json_data: serde_json::Value, select_colmun: &str) -> anyhow::Result<()> {
+    // println!("Hello, world!");
+    // // カレントディレクトリのパスを取得する
+    // let current_dir = std::env::current_dir().unwrap();
+    // println!("current_dir: {:?}", current_dir);
 
-// 
 
-// pub fn json_get_text<T: Deserialize>(output_path: &str, data:T) -> Result<(), Box<dyn std::error::Error>> {
-//     // println!("Hello, world!");
-//     // // カレントディレクトリのパスを取得する
-//     // let current_dir = std::env::current_dir().unwrap();
-//     // println!("current_dir: {:?}", current_dir);
+    let mut texts = Vec::<String>::new();
 
-//     let file_path = "data/oasst1_89k_ja.json";
-//     let mut file = File::open(file_path).expect("Failed to open the file");
+    json_data.as_array().unwrap().iter().for_each(|message| {
+        texts.push(
+            message[select_colmun]
+            .clone()
+            .as_str().unwrap()
+            .replace("\n", "")
+        );
+    });
 
-//     // Read the content of the file into a String
-//     let mut file_content = String::new();
-//     file.read_to_string(&mut file_content)
-//         .expect("Failed to read the file");
+    let out_file_path = "data/texts1.csv";
+    println!("{:?}", &texts);
 
-//     // Deserialize the JSON content into your data structure
-//     let parsed_data: Vec<Message> = serde_json::from_str(&file_content)
-//         .expect("Failed to parse JSON");
+    if let Err(err) = write_csv(texts, out_file_path) {
+        eprintln!("Error writing CSV: {}", err);
+    } else {
+        println!("CSV file written successfully!");
+    }
+    // Now you can work with the parsed data
 
-//     let mut texts = Vec::<String>::new();
+    Ok(())
+}
 
-//     parsed_data.iter().for_each(|message| {
-//         texts.push(
-//             message.text_ja
-//             .clone()
-//             .replace("\n", "")
-//         );
-//     });
+fn write_csv(data: Vec<String>, file_path: &str) -> anyhow::Result<()> {
+    let mut file = File::create(file_path)?;
 
-//     let out_file_path = "data/texts1.csv";
+    for line in data {
+        file.write_all(line.as_bytes())?;
+        file.write_all(b",\n")?; // Add a newline after each line
+    }
 
-//     if let Err(err) = write_csv(texts, out_file_path) {
-//         eprintln!("Error writing CSV: {}", err);
-//     } else {
-//         println!("CSV file written successfully!");
-//     }
-
-//     // Now you can work with the parsed data
-//     // println!("{:?}", texts);
-// }
-
-// fn write_csv(data: Vec<String>, file_path: &str) -> anyhow::Result<()> {
-//     let mut file = File::create(file_path)?;
-
-//     for line in data {
-//         file.write_all(line.as_bytes())?;
-//         file.write_all(b",\n")?; // Add a newline after each line
-//     }
-
-//     Ok(())
-// }
+    Ok(())
+}
--- data_processing/src/main.rs
+++ data_processing/src/main.rs
@@ -1,13 +1,33 @@
-// mod lib;
+mod lib;
 
 // use lib::json_get_text;
 
-// use data_processing::json_get_text;
+use lib::json_get_text;
 
 
+const FOLDER_PATH: &str = "static/data";
 
+
+use std::fs::File;
+use std::io::BufReader;
+use std::path::{PathBuf};
 
 fn main() {
-    // json_get_text();
+    let folder = PathBuf::from(FOLDER_PATH);
+    let file_path = folder.join("oasst1_89k_ja.json");
+    let output_path = folder.join("texts1.csv");
+
+    println!("file_path: {:?}", &file_path);
+
+    let file = File::open(file_path).expect("Failed to open the file");
+    let buf_reader = BufReader::new(file);
+    let parsed_data =  serde_json::from_reader(buf_reader).expect("Failed to read the file");
+
+    json_get_text(
+        output_path.to_str().unwrap(),
+        parsed_data,
+        "text_ja"
+    ).unwrap(
+    );
 }
 
